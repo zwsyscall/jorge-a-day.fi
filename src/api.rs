41a81dcd8454008eb693c52f4abe11cf35820734
zwsyscall <61552486+zwsyscall@users.noreply.github.com>
2025-07-14 15:11:56 +0300

    Update API to use image_cache module

--- src/api.rs
+++ src/api.rs
@@ -1,16 +1,16 @@
 use std::sync::Arc;
 
-use crate::config::AppConfig;
-use actix_web::{HttpResponse, Responder, get, http::header::ContentType, web};
+use crate::{cache::CacheTrait, config::AppConfig};
+use actix_web::{HttpRequest, HttpResponse, Responder, get, http::header::ContentType, web};
 use log::error;
 use tokio::sync::Mutex;
 
-use crate::cache::ImageCache;
+use crate::image_cache::cache::Cache;
 
 #[get("/daily")]
 async fn get_daily_image(
     _: web::Data<AppConfig>,
-    cache: web::Data<Arc<Mutex<ImageCache>>>,
+    cache: web::Data<Arc<Mutex<Cache>>>,
 ) -> impl Responder {
     match cache.lock().await.get_newest_image().await {
         Some(image) => HttpResponse::Ok()
@@ -24,11 +24,10 @@ async fn get_daily_image(
 }
 
 #[get("/images")]
-async fn list_images(
-    _: web::Data<AppConfig>,
-    cache: web::Data<Arc<Mutex<ImageCache>>>,
-) -> impl Responder {
-    let images = cache.lock().await.get_images().await;
+async fn list_images(req: HttpRequest, cache: web::Data<Arc<Mutex<Cache>>>) -> impl Responder {
+    let domain = req.full_url();
+
+    let images = cache.lock().await.get_images(&domain.to_string()).await;
     HttpResponse::Ok()
         .content_type(ContentType::json())
         .json(images)
@@ -37,12 +36,12 @@ async fn list_images(
 #[get("/images/{id}")]
 async fn get_image(
     _: web::Data<AppConfig>,
-    cache: web::Data<Arc<Mutex<ImageCache>>>,
+    cache: web::Data<Arc<Mutex<Cache>>>,
     path: web::Path<String>,
 ) -> impl Responder {
     let image_path = path.into_inner();
 
-    match cache.lock().await.fetch_image(&image_path).await {
+    match cache.lock().await.get_data(&image_path).await {
         Ok(image) => HttpResponse::Ok()
             .content_type(image.content_type())
             .body(image.data),
