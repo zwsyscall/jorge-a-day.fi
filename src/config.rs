1b0239d865a168e299731c579e5c0a49f355cd77
zwsyscall <61552486+zwsyscall@users.noreply.github.com>
2025-07-15 12:19:01 +0300

    revamp config, add SSL support

--- src/config.rs
+++ src/config.rs
@@ -1,12 +1,77 @@
+use anyhow::anyhow;
 use confique::Config;
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 use serde::Deserialize;
+use std::path::PathBuf;
 
-pub static CONFIG_PATH: &'static str = "./config.toml";
+pub static CONFIG_PATH: &'static str = "/etc/jorge-a-day/config.toml";
 
 #[derive(Config, Deserialize, Clone)]
 pub struct AppConfig {
     #[config(default = ["."])]
     pub directories: Vec<String>,
+
     #[config(default = 300)]
     pub cache_age: i64,
+
+    #[config(default = "0.0.0.0:8443")]
+    pub address: String,
+
+    #[config(default = false)]
+    pub ssl: bool,
+
+    #[config()]
+    pub cert: Option<String>,
+
+    #[config()]
+    pub key: Option<String>,
+}
+
+impl AppConfig {
+    pub fn check(&self) -> anyhow::Result<(String, String)> {
+        let cert_missing = self.cert.is_none();
+        let key_missing = self.key.is_none();
+
+        if self.ssl && (cert_missing || key_missing) {
+            let mut missing = Vec::new();
+            if cert_missing {
+                missing.push("certificate");
+            }
+            if key_missing {
+                missing.push("key");
+            }
+            anyhow::bail!("SSL is enabled but missing {}!", missing.join(" and "));
+        }
+
+        let cert = self
+            .cert
+            .clone()
+            .ok_or_else(|| anyhow!("Certificate is missing"))?;
+        let key = self.key.clone().ok_or_else(|| anyhow!("Key is missing"))?;
+
+        Ok((cert, key))
+    }
+}
+
+// Takes in the certificate and key and generates an openssl instance.
+// Most likely fail cause is missing certificates or incorrect permissions.
+pub fn create_ssl_builder(
+    cert_path: &str,
+    key_path: &str,
+) -> anyhow::Result<openssl::ssl::SslAcceptorBuilder> {
+    match (
+        PathBuf::from(&cert_path).exists(),
+        PathBuf::from(&key_path).exists(),
+    ) {
+        (false, _) => return Err(anyhow!("Certificate does not exist.")),
+        (_, false) => return Err(anyhow!("Certificate does not exist.")),
+        (_, _) => {}
+    }
+
+    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls())?;
+
+    builder.set_private_key_file(key_path, SslFiletype::PEM)?;
+    builder.set_certificate_chain_file(cert_path)?;
+
+    Ok(builder)
 }
--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,21 @@ mod web_gui;
 
 use actix_web::{App, HttpServer, middleware, web};
 use confique::Config;
+use log::{error, info};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
 use crate::cache::{cache_cleanup, directory_watcher};
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> anyhow::Result<()> {
     // Fix this jibberish
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("debug"));
-    let app_config = config::AppConfig::from_file(config::CONFIG_PATH).unwrap();
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let app_config = config::AppConfig::from_file(config::CONFIG_PATH)?;
+    let ssl_enabled = app_config.ssl;
+    let certificate_bundle = app_config.check();
+
+    let bind_address = app_config.address.to_owned();
 
     let mut cache = image_cache::cache::Cache::from(app_config.cache_age);
     cache.init(&app_config.clone()).await;
@@ -28,19 +33,36 @@ async fn main() -> std::io::Result<()> {
     tokio::spawn(async move { cache_cleanup(shared_cache_clone).await });
     tokio::spawn(async move { directory_watcher(shared_cache_clone_2).await });
 
-    log::info!("Starting server at http://127.0.0.1:8080");
-    HttpServer::new(move || {
+    info!("Starting server at {}", bind_address);
+    let server = HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
             .app_data(web::Data::new(app_config.to_owned()))
             .app_data(web::Data::new(shared_cache.to_owned()))
-            .service(api::get_daily_image)
+            .service(api::daily)
             .service(api::get_image)
             .service(api::list_images)
             .service(web_gui::gallery)
             .service(web_gui::favicon)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    });
+
+    // Start the server with (or without) SSL
+    if ssl_enabled {
+        if let Ok((cert, key)) = certificate_bundle {
+            info!("Starting server with SSL enabled.");
+            let builder = match config::create_ssl_builder(&cert, &key) {
+                Ok(data) => data,
+                Err(e) => {
+                    error!("Error creating TLS instance: {}", e);
+                    panic!("Cannot access certificates!")
+                }
+            };
+            server.bind_openssl(bind_address, builder)?.run().await?;
+        }
+    } else {
+        info!("Starting server without SSL enabled.");
+        server.bind(bind_address)?.run().await?;
+    }
+
+    Ok(())
 }
