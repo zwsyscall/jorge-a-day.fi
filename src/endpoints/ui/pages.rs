311f31bcab65e0ae2b9f54b4cd3c544a271dcb2e
zwsyscall <61552486+zwsyscall@users.noreply.github.com>
2025-07-16 11:39:54 +0300

    Add pages

--- src/endpoints/ui/pages.rs
+++ src/endpoints/ui/pages.rs
@@ -1,49 +1,13 @@
-use std::sync::Arc;
-
-use crate::{cache::CacheTrait, image_cache::cache::Cache};
-use actix_web::{HttpResponse, Responder, get, web};
 use askama::Template;
 
-use tokio::sync::Mutex;
 #[derive(Template)]
-#[template(path = "gallery.html", ext = "html")]
-struct GalleryPage {
-    images: Vec<String>,
+#[template(path = "gallery.html.j2", ext = "html")]
+pub struct GalleryPage {
+    pub images: Vec<String>,
 }
 
-#[get("/")]
-async fn gallery(cache: web::Data<Arc<Mutex<Cache>>>) -> impl Responder {
-    let data: Vec<String> = cache
-        .lock()
-        .await
-        .get_images("images")
-        .await
-        .iter()
-        .map(|i| i.url.to_owned())
-        .collect();
-
-    let page = GalleryPage { images: data };
-    match page.render() {
-        Ok(page) => HttpResponse::Ok().body(page),
-        Err(_) => HttpResponse::InternalServerError().body("Error templating gallery page"),
-    }
-}
-
-#[get("/about")]
-async fn about(cache: web::Data<Arc<Mutex<Cache>>>) -> impl Responder {
-    let image_size = cache.lock().await.len();
-
-    /*let page = AboutPage { images: data };
-    match page.render() {
-        Ok(page) => HttpResponse::Ok().body(page),
-        Err(_) => HttpResponse::InternalServerError().body("Error templating gallery page"),
-    }*/
-    "ok"
-}
-
-#[get("/favicon.ico")]
-async fn favicon() -> impl Responder {
-    HttpResponse::Ok()
-        .content_type("image/vnd.microsoft.icon")
-        .body(&include_bytes!("../../../static/favicon.ico")[..])
+#[derive(Template)]
+#[template(path = "about.html.j2", ext = "html")]
+pub struct AboutPage {
+    pub image_count: usize,
 }
